5bfd93f25dbfbdd162c0972fb13fbc8364f18fe4
Shaher_ben_moussa <shahirbenmoussa06@gmail.com>
2025-11-22 22:56:48 +0200

    Create Member.java

---
+++ modules/Member.java
@@ -0,0 +1,15 @@
+package modules;
+
+public class Member extends User {
+
+    private String subscription;
+
+    public Member(int id, String name, String phone, String username, String password, String subscription) {
+        super(id, name, phone, username, password, "member");
+        this.subscription = subscription;
+    }
+
+    public void showSubscription() {
+        System.out.println("Subscription: " + subscription);
+    }
+}
