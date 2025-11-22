cbe9993cbb614f95e6c9f989896138eca5450734
Shaher_ben_moussa <shahirbenmoussa06@gmail.com>
2025-11-23 01:21:54 +0200

    Update UserManager.java

--- modules/UserManager.java
+++ modules/UserManager.java
@@ -4,22 +4,35 @@ import java.util.ArrayList;
 
 public class UserManager {
 
+    // دي الليست الأساسية اللي بنخزن فيها كل اليوزرز اللي موجودين في النظام
+    // سواء كانوا Admin أو Member أو Coach
     private ArrayList<User> users = new ArrayList<>();
 
-    public ArrayList<User> getAllUsers() {
+    // دي بترجع الليست كلها علشان تتخزن في ملف لما نعمل save
+    // أو لو حد في المشروع محتاج يشوف كل اليوزرز
+    public ArrayList<User> getAllUsers() { 
         return users;
     }
 
+    // دي وظيفة إضافة يوزر جديد للنظام
+    // التيم هيستخدمها لما الادمن يضيف Member أو يضيف Coach
     public void addUser(User u) {
         users.add(u);
     }
 
+    // دي وظيفة تسجيل الدخول
+    // بتلف على كل اليوزرز وتشيّك هل اليوزر والباسورد اللي اتكتبوا صح ولا لأ
+    // لو لقت يوزر مطابق → بترجعه
+    // لو مفيش حد مطابق → بترجع null
     public User login(String username, String password) {
         for (User u : users) {
-            if (u.login(username, password)) {
-                return u;
+
+            // ال login هنا اللي جوّا User class نفسه
+            if (u.login(username, password)) { 
+                return u; // لو لقى يوزر صح بيرجعه
             }
         }
-        return null; 
+        return null; // لو مفيش ولا واحد مظبوط
     }
 }
+
