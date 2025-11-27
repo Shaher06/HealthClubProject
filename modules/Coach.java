d3748d25e6986f8fc0504de4ed7f01cdc16af45e
na3173285-lab <na3173285@gmail.com>
2025-11-28 00:42:17 +0200

    Update Coach.java

--- modules/Coach.java
+++ modules/Coach.java
@@ -1,12 +1,51 @@
 package modules;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author LENOVO
+ */
+import java.util.*;
+import java.io.IOException;
+
 public class Coach extends User {
 
-    public Coach(int id, String name, String phone, String username, String password) {
-        super(id, name, phone, username, password, "coach");
+    public Coach(int id, String name, String phone, String username, String password, String role) {
+        super(id, name, phone, username, password, role);
+
     }
 
-    public void coachMenu() {
-        System.out.println("Coach menu will be here...");
+    public void addPlan(int memberId, String plan) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\LENOVO\\Downloads\\mavenproject2\\src\\main\\java\\data\\plans.txt", true));
+            pw.println(memberId + "," + plan);
+            System.out.println("Plan added successfully for member ID: " + memberId);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    public void addSheduale(int memberId, String schedule) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\LENOVO\\Downloads\\mavenproject2\\src\\main\\java\\data\\sheduals.txt", true));
+            pw.println(memberId + "," + schedule);
+        } catch (IOException e) {
+            System.out.println("Error saving schedule: " + e.getMessage());
+        }
+
+    }
+    
+    
+    public void sendMessageToAllMembers(String message, ArrayList<User> allUsers) {
+        for (User u : allUsers) {
+            if (u.getRole().equals("member")) {
+                Notification n = new Notification(this.name, message); // لازم Notification يكون موجود
+              u.receiveNotification(n);
+            }
+        }
+        System.out.println("Message sent to all members!");
+    }
+
 }
